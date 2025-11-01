0edb53ba33d032db1c6bde893da77053a6154197
Shubham-Bhatt15 <sb6642182@gmail.com>
2025-11-01 19:28:22 +0530

    commit 2

---
+++ leetcode/linkedlist/merge_two_sorted _list.cpp
@@ -0,0 +1,64 @@
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode() : val(0), next(nullptr) {}
+ *     ListNode(int x) : val(x), next(nullptr) {}
+ *     ListNode(int x, ListNode *next) : val(x), next(next) {}
+ * };
+ */
+class Solution {
+public:
+    ListNode* mergeTwoLists(ListNode* list1, ListNode* list2) {
+        ListNode* head = NULL,*current = NULL;
+        if(list1==NULL&&list2==NULL) return NULL;
+        if(list1==NULL&&list2!=NULL) return list2;
+        if(list1!=NULL&&list2==NULL) return list1;
+        
+        while(list1!=NULL&&list2!=NULL){
+            if(head==NULL){
+                if(list1->val>list2->val){
+                    head = list2;
+                    current = head;
+                    list2 = list2->next;
+                }
+                else{
+                    head = list1;
+                    current = head;
+                    list1 = list1->next;
+                }
+            }
+            else{
+                if(list1->val>list2->val){
+                    current->next = list2;
+                    list2 = list2->next;
+
+                    current=current->next;
+                }
+                else{
+                    current->next = list1;
+                    list1 = list1->next;
+
+                    current=current->next;
+                
+                }
+
+            }
+            
+        }
+        while(list1!=NULL){
+            current->next = list1;
+            list1 = list1->next;
+            current = current->next;
+        }
+        while(list2!=NULL){
+            current->next = list2;
+            list2 = list2->next;
+            current = current->next;
+        }
+    
+        return head;
+        
+    }
+};
