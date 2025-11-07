44f8294355dcc55996f6f9c8ff5db33e03707116
Shubham-Bhatt15 <sb6642182@gmail.com>
2025-11-07 07:43:20 +0530

    seventh

---
+++ linked_list_cycle.cpp
@@ -0,0 +1,29 @@
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode(int x) : val(x), next(NULL) {}
+ * };
+ */
+class Solution {
+public:
+    bool hasCycle(ListNode *head) {
+        if (head==NULL){
+            return false;
+            
+        }
+        ListNode * q = head;
+        ListNode * p = head;
+
+        while(p!=NULL && p->next!= NULL ){
+            p = (p->next)->next;
+            q=q->next;
+            if(p==q)
+                return true;
+        }
+     return false;
+        
+    }
+
+};
